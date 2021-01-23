2e19f79f5b4e1d441857ba4f31b3a30f19c29edf
Jacob Ross <survivaldawn@gmail.com>
2021-01-23 07:45:50 -0600

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,16 +1,29 @@
-// Copy and paste into the console to double check. 
+// The nullish coalescing operator (??) is a logical operator that returns its right-hand side operand when its left-hand side 
+// operand is null or undefined, and otherwise returns its left-hand side operand.
 
-var x = 3;
-var y = 8;
+const nullValue = null;
+const emptyText = ""; // falsy
+const someNumber = 42;
 
-!(x == "3" || x === y) && !(y != 8 && x <= y);
+const valA = nullValue ?? "default for A";
+const valB = emptyText ?? "default for B";
+const valC = someNumber ?? 0;
 
-// false
+console.log(valA); // "default for A"
+console.log(valB); // "" (as the empty string is not null or undefined)
+console.log(valC); // 42
 
-var str = "" 
-var msg = "haha!" 
-var isFunny = "false" 
+// ----------
 
-!((str || msg) && isFunny); 
+function A() { console.log('A was called'); return undefined;}
+function B() { console.log('B was called'); return false;}
+function C() { console.log('C was called'); return "foo";}
 
-// false
+console.log( A() ?? C() );
+// logs "A was called" then "C was called" and then "foo"
+// as A() returned undefined so both expressions are evaluated
+
+console.log( B() ?? C() );
+// logs "B was called" then "false"
+// as B() returned false (and not null or undefined), the right
+// hand side expression was not evaluated
